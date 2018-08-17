1271bda0656eec0941053c94b37a5f8aef514ea1
abhisek <abknayak26@gmail.com>
2018-08-17 18:07:20 +0530

    added broadcasting

--- public/js/index.js
+++ public/js/index.js
@@ -7,10 +7,7 @@ socket.on('disconnect',function(){
     console.log('disconnected from server');
 });
 
-socket.emit('createMessage',{
-    from:"Abhi",
-    text:"this is done"
-});
+
 
 socket.on('newMessage',function(message){
     console.log('message',message);
--- server/server.js
+++ server/server.js
@@ -21,13 +21,14 @@ io.on('connection',(socket)=>{
 
     socket.on('createMessage',(message)=>{
         console.log('createMessage',message);
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt:new Date().getTime()
+        });
     });
 
-    socket.emit('newMessage',{
-        from:"john",
-        text:"see you",
-        createAt:123
-    });
+    
 });
 
 server.listen(port,()=>{
