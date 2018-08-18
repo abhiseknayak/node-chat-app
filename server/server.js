6a1b1ea17559f0780824eb268da1357047b6411e
abhisek <abknayak26@gmail.com>
2018-08-18 11:10:42 +0530

    added form and incomming messages

--- server/server.js
+++ server/server.js
@@ -25,9 +25,10 @@ io.on('connection',(socket)=>{
 
     socket.broadcast.emit('newMessage',generateMessage('Admin','A new user joined'));
 
-    socket.on('createMessage',(message)=>{
+    socket.on('createMessage',(message,callback)=>{
         console.log('createMessage',message);
         io.emit('newMessage',generateMessage(message.from,message.text));
+        callback();
     });
 
     
