9191a5357641ba6f5d27e1f45c0bda663f5334d2
abhisek <abknayak26@gmail.com>
2018-08-19 13:31:56 +0530

    send messages to people in the same grp only

--- public/js/chat.js
+++ public/js/chat.js
@@ -72,7 +72,7 @@ var messageTextBox=jQuery('[name=message]');
 jQuery('#message-form').on('submit',function(e){
     e.preventDefault();
     socket.emit('createMessage',{
-        from:"User",
+        
         text:messageTextBox.val()
     },function(){
         messageTextBox.val('');
